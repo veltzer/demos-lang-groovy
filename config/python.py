2f155ec12d2710acad08b6100096531a97667538
Mark Veltzer <mark.veltzer@gmail.com>
2024-01-12 15:06:02 +0200

    more python.py changes

--- config/python.py
+++ config/python.py
@@ -4,11 +4,11 @@ from typing import List
 config_requires: List[str] = []
 dev_requires: List[str] = []
 install_requires: List[str] = []
-make_requires: List[str] = [
+build_requires: List[str] = [
     "pymakehelper",
     "pydmt",
     "pyclassifiers",
     "pylint",
 ]
 test_requires: List[str] = []
-requires = config_requires + install_requires + make_requires + test_requires
+requires = config_requires + install_requires + build_requires + test_requires

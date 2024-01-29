235e2f6943aa33783009d30c6fa18333e8c2ac2d
Mark Veltzer <mark.veltzer@gmail.com>
2024-01-29 16:41:03 +0200

    no commit message given

---
+++ src/lists/change_type.groovy
@@ -0,0 +1,14 @@
+#!/usr/bin/groovy
+
+l=7.6
+println l
+l=[]
+println l
+l=true
+println l
+
+if random()<0.5 {
+	l=5.6
+} else {
+	l=false
+}
---
+++ src/lists/untyped.groovy
@@ -0,0 +1,7 @@
+#!/usr/bin/groovy
+
+l=["one", 3.14, ["apple", "orange"], true]
+l2=[]
+for(x in l) {
+	println x
+}

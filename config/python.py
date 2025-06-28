a52d8d8a69028a635506e736ab7c709175fcb08f
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-28 10:09:03 +0300

    fixed up deps

--- config/python.py
+++ config/python.py
@@ -1,14 +1,8 @@
-from typing import List
+""" python deps for this project """
 
-
-config_requires: List[str] = []
-dev_requires: List[str] = []
-install_requires: List[str] = []
-build_requires: List[str] = [
-    "pymakehelper",
+build_requires: list[str] = [
     "pydmt",
-    "pyclassifiers",
+    "pymakehelper",
     "pylint",
 ]
-test_requires: List[str] = []
-requires = config_requires + install_requires + build_requires + test_requires
+requires = build_requires
